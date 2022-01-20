5a0c97bd7f30d9999ce21e83dde1076fa9a493da
viettu2k <doanviettu2k@gmail.com>
2022-01-20 21:08:08 +0700

    display content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing';
